62dd15c913b4691c031eaa857d2c6c12a481acff
Souvikmanna2005 <souvik2005110@gmail.com>
2025-11-10 21:53:05 +0530

    Souvik solve problem 10

---
+++ problem10.py
@@ -0,0 +1,12 @@
+# SOUVIK MANNA
+
+# 10. Given two lists a = [1, 2, 3] and b = [4, 5, 6], use list comprehension to create a list containing the sum of corresponding elements.
+
+import numpy as np
+
+a = np.array([1,2,3])
+b = np.array([4,5,6])
+
+sum = a+b
+
+print(f'{a} \n + \n{b} = ', sum)  #bojhar jonno eivabe \n use kore dekhiye6i because ekhane matrix addition use kore6i
