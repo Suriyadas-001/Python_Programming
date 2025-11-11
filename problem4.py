c457a420cdf8dfaf4fd00031167e279ba25728b6
prakashmaiti09-arch <prakashmaiti09@gmail.com>
2025-11-11 16:07:38 +0530

    Prakash solve problem 4

---
+++ problem4.py
@@ -0,0 +1,8 @@
+# PRAKASH MAITI
+
+# 4. Given numbers = [1, 2, 3, 4, 5], use map() with a lambda to cube all numbers.
+
+numbers = [1, 2, 3, 4, 5]
+cubed_numbers = list(map(lambda x: x**3, numbers))
+
+print(cubed_numbers)   # <-- This line shows the output
